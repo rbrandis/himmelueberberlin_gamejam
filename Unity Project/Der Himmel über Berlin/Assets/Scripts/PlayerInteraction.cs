d50c17141c2e82459218ddd3b4a4e06afd9a64c8
Rüdiger Brandis <post@rbrandis.eu>
2018-11-21 15:55:02 +0100

    Added Raycast Object Detect

---
+++ Unity Project/Der Himmel über Berlin/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InteractRaycast();
+	}
+
+    void InteractRaycast()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit; 
+
+        if(Physics.Raycast(ray, out hit, 5.0f))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+        }
+        else
+        {
+            Debug.Log("-");
+        }
+    }
+}
