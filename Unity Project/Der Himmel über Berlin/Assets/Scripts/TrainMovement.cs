378adb941ba2935df35aa512d84a6bdd0410e5b4
Rüdiger Brandis <post@rbrandis.eu>
2018-11-21 16:30:56 +0100

    Added Train Movement Script (not working yet)

---
+++ Unity Project/Der Himmel über Berlin/Assets/Scripts/TrainMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMovement : MonoBehaviour {
+
+    public Transform[] Target;
+    public float Speed;
+
+    private int _current; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position != Target[_current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, Target[_current].position, Speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else _current = (_current + 1) % Target.Length;
+    }
+}
